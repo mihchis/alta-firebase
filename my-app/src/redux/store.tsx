315b18b13f7b8873c9d891872fbc8b9824dca6c0
mihchis <trin79136@gmail.com>
2022-02-28 23:04:13 +0700

    hoan thanh giao dien

---
+++ my-app/src/redux/store.tsx
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import paginationSlice from '../components/paginationSlice';
+import equipSlice from '../Pages/equipment/equipSlice';
+import AccountSlice from '../Pages/manage/Account/AccountSlice';
+import randomSlice from '../Pages/random/randomSlice';
+import ServiceSlice from '../pages/service/ServiceSlice';
+import userSlice from '../Pages/user/userSlice';
+
+const store = configureStore({
+  reducer: {
+    user: userSlice.reducer,
+    equipment: equipSlice.reducer,
+    pagination: paginationSlice.reducer,
+    account: AccountSlice.reducer,
+    service: ServiceSlice.reducer,
+    random: randomSlice.reducer,
+  },
+});
+export default store;
